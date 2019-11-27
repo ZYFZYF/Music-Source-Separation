0ab9b161f565146e0ac5229c7ebf698a381efbc0
ZYFZYF <973446154@qq.com>
2019-11-27 11:25:30 +0800

    构建堆叠沙漏网络

--- Model.py
+++ Model.py
@@ -17,17 +17,18 @@ class Full(nn.Module):
         return self.model(x)
 
 
+# 这里隐藏了一个条件就是卷积层过后每个feature map大小不变，所以要根据kernel_size和stride的值来自己设置padding
 class Conv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, do_activation=True):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, do_activation=True):
         super(Conv, self).__init__()
         if not do_activation:
             self.model = nn.Sequential(
                 nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride,
-                          padding=padding))
+                          padding=(kernel_size - 1) // 2))
         else:
             self.model = nn.Sequential(
                 nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride,
-                          padding=padding),
+                          padding=(kernel_size - 1) // 2),
                 nn.BatchNorm2d(num_features=out_channels),
                 nn.ReLU())
 
@@ -56,6 +57,40 @@ class Hourglass(nn.Module):
         return self.skip(x) + self.model(x)
 
 
+class StackedHourglassNet(nn.Module):
+    def __init__(self, num_stacks, input_channels, output_channes, next_depth_add_channels):
+        super(StackedHourglassNet, self).__init__()
+        self.num_stacks = num_stacks
+        self.prepare = nn.Sequential(Conv(1, 64, kernel_size=7, stride=1),
+                                     # TODO 这里PosNet是stride为3，padding为2，之后可以试试，先保持和SHNet一样
+                                     Conv(64, 128),  # 这里对于原来PosNet来说少了个下采样MaxPooling，我觉得是合理的
+                                     Conv(128, 128),
+                                     Conv(128, input_channels))
+        # 堆叠沙漏模块
+        self.hourglass = nn.ModuleList(
+            nn.Sequential(Hourglass(depth=4, channels=input_channels, next_depth_add_channels=next_depth_add_channels),
+                          Conv(input_channels, input_channels),
+                          Conv(input_channels, input_channels, 1)) for i in range(num_stacks))
+        # 输出Mask的上面的path
+        self.output = nn.ModuleList(Conv(input_channels, output_channes) for i in range(num_stacks))
+        # 输向下一个沙漏的features的下面的path
+        self.next = nn.ModuleList(Conv(input_channels, input_channels) for i in range(num_stacks - 1))
+        # 合并上面和下面，所以这里相当于下一个网络在拟合残差
+        self.merge = nn.ModuleList(Conv(output_channes, input_channels) for i in range(num_stacks - 1))
+
+    def forward(self, x):
+        x = self.prepare(x)
+        # 要输出多个堆叠网络的结果
+        predicts = []
+        for i in range(self.num_stacks):
+            x = self.hourglass[i](x)
+            predicts.append(self.output[i](x))
+            if i != self.num_stacks - 1:
+                x = self.merge(predicts[-1]) + self.next(x)
+        # 因为第一维是batch
+        return torch.stack(predicts, 1)
+
+
 if __name__ == "__main__":
     full = Full(input_dim=10, output_dim=100)
     print(full)
@@ -63,7 +98,7 @@ if __name__ == "__main__":
     print(conv)
     hourglass = Hourglass(depth=4, channels=64, next_depth_add_channels=64)
     print(hourglass)
-    input = torch.rand(3,64,128,128)
+    input = torch.rand(3, 64, 128, 128)
     output = hourglass(input)
     print(input)
     print(output)
---
+++ Utils.py
@@ -0,0 +1,37 @@
+import os
+import glob
+import librosa
+import numpy as np
+
+
+def mir_1k_data_generator(train):
+    for wav in glob.glob('Dataset/MIR-1K/Wavfile/*.wav'):
+        filename = os.path.split(wav)[1]
+        if (filename.startswith('abjones') or filename.startswith('amy')) == train:
+            origin_source, _ = librosa.load(wav, sr=8000, mono=False)  # TODO 这里的采样率，该文中用了8k，实际上原音频是16k
+            mixed_source_origin = librosa.to_mono(origin_source)
+            left_source_origin = origin_source[0]
+            right_source_origin = origin_source[1]
+            # print(np.min(left_source), np.min(right_source), np.min(mixed_source)) TODO 为什么声压有负的？
+            mixed_source_magnitude_spectrum = np.abs(to_spectrum(mixed_source_origin))
+            left_source_magnitude_spectrum = np.abs(
+                to_spectrum(np.asfortranarray(left_source_origin)))  # 以前是不需要做这一步的来让flags['F_CONTIGUOUS']=True的
+            right_source_magnitude_spectrum = np.abs(to_spectrum(np.asfortranarray(right_source_origin)))
+
+            # 归一化 # TODO 可以试试不做归一化会怎么样
+            max_value = np.max(mixed_source_magnitude_spectrum)
+            mixed_source_magnitude_spectrum = mixed_source_magnitude_spectrum / max_value
+            left_source_magnitude_spectrum = left_source_magnitude_spectrum / max_value
+            right_source_magnitude_spectrum = right_source_magnitude_spectrum / max_value
+            # TODO 原文只用了幅度来做，能不能把相位也加进来
+
+            yield left_source_origin, right_source_origin, mixed_source_origin, left_source_magnitude_spectrum, right_source_magnitude_spectrum, mixed_source_magnitude_spectrum
+
+
+def to_spectrum(wav, window_size=1024, hop_size=256):
+    return librosa.stft(wav, n_fft=window_size, hop_length=hop_size)
+
+
+if __name__ == "__main__":
+    for x, y, z, w, u, v in mir_1k_data_generator(train=True):
+        print(len(x), len(y), len(z))
