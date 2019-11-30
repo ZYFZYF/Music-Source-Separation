26ec0ee933834b29b0056ec78576195a94639b3b
ZYFZYF <973446154@qq.com>
2019-11-30 20:59:37 +0800

    只预测1个

--- predict_one.py
+++ predict_one.py
@@ -9,23 +9,20 @@ import os
 
 MAX_ITERATIONS = 1000  # 进行这么多batch的训练
 BATCH_SIZE = 4  # 每个batch的大小
-
-NUM_STACKS = 4  # 堆叠沙漏网络的层数
-FIRST_DEPTH_CHANNELS = 64  # 沙漏网络第一层的通道数
-OUTPUT_CHANNELS = 2  # 整个网络输出的通道数
-NEXT_DEPTH_ADD_CHANNELS = 0  # 沙漏网络中每下一层增加的通道数
-
 TRAIN_SAVE_POINT = 50  # 保存点
-
 TEST_STEP = 20  # 测试时
 TOTAL_TEST = 800  # 总共测多少条
+
+NUM_STACKS = 1  # 堆叠沙漏网络的层数
+FIRST_DEPTH_CHANNELS = 64  # 沙漏网络第一层的通道数
+NEXT_DEPTH_ADD_CHANNELS = 64  # 沙漏网络中每下一层增加的通道数
+OUTPUT_CHANNELS = 1  # 整个网络输出的通道数
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print('train device: {}'.format(device))
 
 
 def judge(input, output, predict):
-    # 每个堆叠沙漏网络的输出的loss和
     return sum(torch.mean(torch.abs(predict[x].mul(input) - output)) for x in range(NUM_STACKS))
 
 
@@ -60,7 +57,7 @@ def train():
     print("-------------------begin training...----------------------")
     for i in tqdm.tqdm(range(MAX_ITERATIONS)):
         input = torch.empty(BATCH_SIZE, 1, 512, 64)
-        output = torch.empty(BATCH_SIZE, 2, 512, 64)
+        output = torch.empty(BATCH_SIZE, 1, 512, 64)
         # 每个batch都是从所有训练数据中随机得到的
         for j in range(BATCH_SIZE):
             index = np.random.randint(len(train_data))  # np.random.randint()是左闭右开
@@ -68,7 +65,6 @@ def train():
             start = np.random.randint(mixed_mag.shape[-1] - 64)
             input[j, 0, :, :] = torch.from_numpy(mixed_mag[:512, start:start + 64])
             output[j, 0, :, :] = torch.from_numpy(left_mag[:512, start:start + 64])
-            output[j, 1, :, :] = torch.from_numpy(right_mag[:512, start:start + 64])
             input = input.to(device)
             output = output.to(device)
         optimizer.zero_grad()
@@ -103,7 +99,6 @@ def test():
         srcLen = mix_mag.shape[-1]
         startIndex = 0
         predict_left = np.zeros((512, srcLen), dtype=np.float32)
-        predict_right = np.zeros((512, srcLen), dtype=np.float32)
         # 除了第一次和最后一次，其余计算时都要用上上下文的信息，然后只提取中间长度为32的结果作为预测的结果
         start = time.time()
         while startIndex + 64 < srcLen:
@@ -114,34 +109,26 @@ def test():
                 output = net(torch.from_numpy(input).to(device))[-1].data.cpu().numpy()  # 取最后一个沙漏的输出作为输出，并且要拷贝到内存里
                 for i in range(4):
                     predict_left[:, startIndex + i * 32 + 16: startIndex + i * 32 + 48] = output[i, 0, :, 16:48]
-                    predict_right[:, startIndex + i * 32 + 16: startIndex + i * 32 + 48] = output[i, 1, :, 16:48]
                 startIndex += 128
             else:
                 input[0, 0, :, :] = mix_mag[0:512, startIndex:startIndex + 64]
                 output = net(torch.from_numpy(input).to(device))[-1].data.cpu().numpy()  # 取最后一个沙漏的输出作为输出，并且要拷贝到内存里
                 if startIndex == 0:
                     predict_left[:, 0:64] = output[0, 0, :, :]
-                    predict_right[:, 0:64] = output[0, 1, :, :]
                 else:
                     predict_left[:, startIndex + 16: startIndex + 48] = output[0, 0, :, 16:48]
-                    predict_right[:, startIndex + 16:startIndex + 48] = output[0, 1, :, 16:48]
                 startIndex += 32
         input[0, 0, :, :] = mix_mag[0:512, srcLen - 64:srcLen]
         output = net(torch.from_numpy(input).to(device))[-1].data.cpu().numpy()  # 取最后一个沙漏的输出作为输出，并且要拷贝到内存里
         length = srcLen - startIndex - 16
         predict_left[:, startIndex + 16:srcLen] = output[0, 0, :, 64 - length:64]
-        predict_right[:, startIndex + 16:srcLen] = output[0, 1, :, 64 - length:64]
         # 有NaN的话<号以及librosa会出问题
         np.nan_to_num(predict_left)
-        np.nan_to_num(predict_right)
         predict_left[np.where(predict_left < 0)] = 0
-        predict_right[np.where(predict_right < 0)] = 0
         predict_left = predict_left * mix_mag[0:512, :] * max_value
-        predict_right = predict_right * mix_mag[0:512, :] * max_value
         predict_left_wav = Utils.to_wav(predict_left, mix_spec_phase[0:512, :])
-        predict_right_wav = Utils.to_wav(predict_right, mix_spec_phase[0:512, :])
         predict_left_wav = librosa.resample(predict_left_wav, 8000, 16000)
-        predict_right_wav = librosa.resample(predict_right_wav, 8000, 16000)
+        predict_right_wav = mix_origin - predict_left_wav
         nsdr, sir, sar, lens = Utils.bss_eval(mix_origin, left_origin, right_origin, predict_left_wav,
                                               predict_right_wav)
         totalLen = totalLen + lens
